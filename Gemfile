58e2fa995e315cde1ede7286906d9e9c56a7304e
teru75 <love.akg.75@gmail.com>
2020-03-04 15:55:25 +0900

    [Add]commentmodel

--- Gemfile
+++ Gemfile
@@ -66,6 +66,3 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails'
 gem 'dotenv-rails'
 
-group :production do
-  gem 'mysql2'
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_035817) do
+ActiveRecord::Schema.define(version: 2020_03_04_063812) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
